5f85d8043d853da58b07744a127687d6b183aa5c
AnsellC <ansellcruz@gmail.com>
2019-08-17 20:43:54 +0800

    phpcs fixer

--- src/Resources/Series.php
+++ src/Resources/Series.php
@@ -6,75 +6,75 @@ use Dawson\TVDB\TVDB;
 
 class Series extends TVDB
 {
-	/**
-	 * Series ID
-	 * 
-	 * @var integer
-	 */
-	public $id;
+    /**
+     * Series ID
+     *
+     * @var integer
+     */
+    public $id;
 
-	/**
-	 * Constructor
-	 */
-	public function __construct(int $id)
-	{
-		$this->id = $id;
-	}
+    /**
+     * Constructor
+     */
+    public function __construct(int $id)
+    {
+        $this->id = $id;
+    }
 
-	/**
-	 * Get the Series
-	 * 
-	 * @return json
-	 */
-	public function get()
-	{
-		try {
-			return json_decode($this->client()->login()->client->get('series/' . $this->id)->getBody());
-		} catch(ClientException $e) {
-			throw new TVDBException($e);
-		}
-	}
+    /**
+     * Get the Series
+     *
+     * @return json
+     */
+    public function get()
+    {
+        try {
+            return json_decode($this->client()->login()->client->get('series/' . $this->id)->getBody());
+        } catch (ClientException $e) {
+            throw new TVDBException($e);
+        }
+    }
 
-	/**
-	 * Series Actors
-	 * 
-	 * @return json
-	 */
-	public function actors()
-	{
-		try {
-			return json_decode($this->client()->login()->client->get('series/' . $this->id . '/actors')->getBody());
-		} catch(ClientException $e) {
-			throw new TVDBException($e);
-		}
-	}
+    /**
+     * Series Actors
+     *
+     * @return json
+     */
+    public function actors()
+    {
+        try {
+            return json_decode($this->client()->login()->client->get('series/' . $this->id . '/actors')->getBody());
+        } catch (ClientException $e) {
+            throw new TVDBException($e);
+        }
+    }
 
-	/**
-	 * Series Episodes
-	 * 
-	 * @return json
-	 */
-	public function episodes()
-	{
-		try {
-			return json_decode($this->client()->login()->client->get('series/' . $this->id . '/episodes')->getBody());
-		} catch(ClientException $e) {
-			throw new TVDBException($e);
-		}
-	}
+    /**
+     * Series Episodes
+     *
+     * @return json
+     */
+    public function episodes()
+    {
+        try {
+            return json_decode($this->client()->login()->client->get('series/' . $this->id . '/episodes')->getBody());
+        } catch (ClientException $e) {
+            throw new TVDBException($e);
+        }
+    }
 
-	/**
-	 * Series Images
-	 * 
-	 * @return json
-	 */
-	public function images()
-	{
-		try {
-			return json_decode($this->client()->login()->client->get('series/' . $this->id . '/images')->getBody());
-		} catch(ClientException $e) {
-			throw new TVDBException($e);
-		}
+    /**
+     * Series Images
+     *
+     * @return json
+     */
+    public function images()
+    {
+        try {
+            return json_decode($this->client()->login()->client->get('series/' . $this->id . '/images')->getBody());
+        } catch (ClientException $e) {
+            throw new TVDBException($e);
+        }
     }
     /**
      * Series Wallpapers
@@ -83,26 +83,25 @@ class Series extends TVDB
      */
     public function wallpapers()
     {
-            try {
-                    return json_decode($this->client()->login()->client->get('series/' . $this->id . '/images/query?keyType=fanart')->getBody());
-            } catch(ClientException $e) {
-                    throw new TVDBException($e);
-            }
+        try {
+            return json_decode($this->client()->login()->client->get('series/' . $this->id . '/images/query?keyType=fanart')->getBody());
+        } catch (ClientException $e) {
+            throw new TVDBException($e);
+        }
     }
 
     /**
      * Series Posters
-     * 
+     *
      * @return json
      */
 
     public function posters()
     {
-            try {
-                    return json_decode($this->client()->login()->client->get('series/' . $this->id . '/images/query?keyType=poster')->getBody());
-            } catch(ClientException $e) {
-                    throw new TVDBException($e);
-            }
+        try {
+            return json_decode($this->client()->login()->client->get('series/' . $this->id . '/images/query?keyType=poster')->getBody());
+        } catch (ClientException $e) {
+            throw new TVDBException($e);
+        }
     }
-
 }
